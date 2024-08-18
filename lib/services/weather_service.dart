8d0d64da56bd5d3e7bf70d03986f7493ccbccadc
boshranofal <boshranofal66@gmail.com>
2024-08-18 16:10:33 +0300

    add asyn

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,14 +3,24 @@ import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String apiKey = '580d2c8407a8493b9ae71231241608';
   WeatherService({
     required this.dio,
   });
-  Future getWeather({required String cityName}) async {
-    Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=580d2c8407a8493b9ae71231241608&q=$cityName&days=10');
 
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel> getWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=10');
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data['error']['message'] ??
+          'oops something went wrong, please try again';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception('oops something went wrong, please try again');
+    }
   }
 }
