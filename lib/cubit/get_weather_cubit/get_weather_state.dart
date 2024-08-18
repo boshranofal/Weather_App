b6c90dcc93f4d16afb2d4e6365cb8565dc93f96b
boshranofal <boshranofal66@gmail.com>
2024-08-18 16:26:55 +0300

    add weather state

---
+++ lib/cubit/get_weather_cubit/get_weather_state.dart
@@ -0,0 +1,7 @@
+class GetWeatherState {}
+
+class NoWeatherState extends GetWeatherState {}
+
+class WeatherLodedState extends GetWeatherState {}
+
+class WeatherFailedState extends GetWeatherState {}
