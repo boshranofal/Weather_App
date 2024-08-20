28a31ac02d749c22f0557c7acec30b1aa753316c
boshranofal <boshranofal66@gmail.com>
2024-08-20 12:12:29 +0300

    add prametere

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,11 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class WeatherModel {
   final String cityName;
-  final String date;
+  final DateTime date;
   final String? image;
   final double tempe;
   final double maxtemp;
   final double mintemp;
   final String condition;
+  final int humidity;
+  final double windSpeed;
 
   WeatherModel({
     required this.cityName,
@@ -15,16 +18,20 @@ class WeatherModel {
     required this.maxtemp,
     required this.mintemp,
     required this.condition,
+    required this.humidity,
+    required this.windSpeed,
   });
   factory WeatherModel.fromJson(json) {
     return WeatherModel(
       cityName: json['location']['name'],
-      date: json['current']['last_updated'],
+      date: DateTime.parse(json['current']['last_updated']),
       image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
       tempe: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
       maxtemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
       mintemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
       condition: json['forecast']['forecastday'][0]['day']['condition']['text'],
+      humidity: json['forecast']['forecastday'][0]['day']['avghumidity'],
+      windSpeed: json['forecast']['forecastday'][0]['day']['maxwind_kph'],
     );
   }
 }
