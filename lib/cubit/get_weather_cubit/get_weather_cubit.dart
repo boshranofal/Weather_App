d22d049f8edb013816e73cdd8d256588e84b1002
boshranofal <boshranofal66@gmail.com>
2024-08-20 12:11:54 +0300

    add getweatherCubit

--- lib/cubit/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubit/get_weather_cubit/get_weather_cubit.dart
@@ -1,6 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubit/get_weather_cubit/get_weather_state.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class GetWeatherCubit extends Cubit<GetWeatherState> {
-  GetWeatherCubit(super.initialState);
+  GetWeatherCubit() : super(NoWeatherState());
+  WeatherModel? weatherModel;
+  getWeather({required String cityName}) async {
+    try {
+      weatherModel =
+          await WeatherService(dio: Dio()).getWeather(cityName: cityName);
+      emit(WeatherLodedState(weatherModel: weatherModel!));
+    } catch (e) {
+      emit(WeatherFailedState());
+    }
+  }
 }
